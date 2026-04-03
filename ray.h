be9a57289a29d4b893cf3d2327210e363150bbd4
Adithya Nair <adithyanair121@gmail.com>
2026-04-03 13:09:04 +0530

    ray.h addition

---
+++ ray.h
@@ -0,0 +1,16 @@
+#ifndef RAY_H_
+#define RAY_H_
+
+#include "vec3.h"
+
+typedef struct {
+    point3 orig;
+    vec3 dir;
+} ray;
+
+static point3 ray_at(ray *r, double t) {
+    vec3 scale = (vec3_scale(&(r->dir), t));
+    return vec3_add(&(r->orig), &scale);
+}
+
+#endif // RAY_H_
